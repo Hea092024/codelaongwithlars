b6a0695839f069b001f4a4ddb0460b5f362f4189
Hector Acevedo <HEA092024@gmail.com>
2025-04-07 10:37:09 +0200

    edited main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>error</p>,
+    children: [{ path: "about", element: <div>hei</div> }],
+  },
+  {
+    path: "lars",
+    element: <div>lars</div>,
+  },
+  { path: "*", element: <div>404 not found</div> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
